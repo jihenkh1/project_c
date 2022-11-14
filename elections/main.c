e384c753f1c16a3aaeccde8f2929221c041db24c
jihenkh1 <jihen.khelifi@esprit.tn>
2022-11-14 11:40:03 +0100

    modifier

--- elections/main.c
+++ elections/main.c
@@ -1,17 +1,20 @@
-#include<stdio.h>
-#include<stdlib.h>
+
 #include "functions.h"
 
 int main()
 {
-election E1 ={{1,10,22},1,8,100,"x"},E2={{2,11,23},2,5,200,"y"},E3;
-int x=ajouter("election.txt",E1);
+//election E1 ={{1,10,22},1,8,100,"x"},E2={{2,11,23},2,5,200,"y"},E3;
+election E1;
+int x;
+
+saisir(&E1);
+x=ajouter("election.txt",E1);
 
 if(x==1)
 printf("\najout d election avec succes");
 else printf("\nechec ajout");
 
-x=modifier("election.txt",1,E2);
+/*x=modifier("election.txt",1,E2);
 if(x==1)
 printf("\nmodification de l election avec succes");
 else printf("\nechec modification");
@@ -21,6 +24,6 @@ printf("\nsuppression de point avec succes");
 else printf("\nechec suppression");
 E3=chercher("election.txt",3);
 if(E3.idEl==-1)
-printf("\nintrouvable");
+printf("\nintrouvable");*/
 return 0;
 }
